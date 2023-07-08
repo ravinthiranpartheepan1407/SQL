25dee997c800637b88938165440f3c5664ab37b0
surenravi1407 <surenravi1407@gmail.com>
2023-07-08 15:31:25 +0300

    SQL: Init

---
+++ createDB.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE tablecreations(
+--     ID INT NOT NULL,
+--     NAME VARCHAR(50)
+-- )
+
+-- INSERT INTO tablecreations(ID, NAME) VALUES (01, 'Ravi');
+
+SELECT * FROM tablecreations;
+
