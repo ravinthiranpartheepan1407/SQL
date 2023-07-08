35a775d5dcc743563817a01a085396f9ff18d786
surenravi1407 <surenravi1407@gmail.com>
2023-07-08 15:47:08 +0300

    SQL:init

--- createDB.sql
+++ createDB.sql
@@ -1,9 +1,9 @@
--- CREATE TABLE tablecreations(
---     ID INT NOT NULL,
---     NAME VARCHAR(50)
--- )
+CREATE TABLE tablecreations(
+    ID INT NOT NULL,
+    NAME VARCHAR(50)
+)
 
--- INSERT INTO tablecreations(ID, NAME) VALUES (01, 'Ravi');
+INSERT INTO tablecreations(ID, NAME) VALUES (01, 'Ravi');
 
 SELECT * FROM tablecreations;
 
